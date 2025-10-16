caddb889fcc9a05482289bcb28a05b6cf482bce7
jackieMboyaAchieng <jacklinemboya075@gmail.com>
2025-10-16 14:17:53 +0300

    commit

---
+++ Learning/variables.py
@@ -0,0 +1,24 @@
+# Variables in Python are used to store data values and can be referenced and manipulated during program execution.
+# A variable is created the moment you first assign a value to it.
+# Variables do not need to be declared with any particular type and can even change type after they have been set.
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+
+# Example of variable assignment
+x = 5               # x is of type int and is assigned the value 5 (stores value 5)
+y = "Hello, World!" # y is of type str and is assigned the value "Hello, World!" (stores value "Hello, World!")
+name = "Alice"    # Variable 'name' stores the string "Alice"
+
+print(x)          # Output: 5
+print(y)          # Output: Hello, World!
+print(name)       # Output: Alice
+
+# Rules for Naming Variables in Python
+# 1. Variable names must start with a letter, digit or the underscore (_) character
+# 2. A variable name cannot start with a number (digit  0-9)
+# 3. Variable names can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# 4. Variable names are case-sensitive (age, Age and AGE are three different variables or myVar and myvar are different)
+# 5. Avoid using Python keywords (e.g., if, else, for) as variable names
+# 6. Use meaningful variable names that describe the data they hold (e.g., age, name, total_price) 
